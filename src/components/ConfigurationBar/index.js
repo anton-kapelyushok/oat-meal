330810c4f9373c46527b5967ca7ae1e2ce8d4cc1
korolm <korolm.spb@gmail.com>
2016-12-17 13:15:47 +0300

    PieChart

--- src/components/ConfigurationBar/index.js
+++ src/components/ConfigurationBar/index.js
@@ -1,30 +1,71 @@
 import React, { Component, PropTypes } from 'react';
 import './index.css';
-import GoalSelector from '../GoalSelector';
+import GoalSelector from '../GoalSelector/index.js';
 
 
 export default class ConfigurationBar extends Component {
     render () {
         return (
             <div className="configuration-bar ">
-                <div className = "sliders-bar item border">
-                    <span className = "sliders-bar-label">
-                        <div>Current Target</div>
-                        <div>{this.props.currentGoal.calories}</div>
-                        <div>Calories</div>
-                    </span>
-                    <GoalSelector
-                        onGoalChanged={this.props.onGoalChanged}
-                        currentGoal={this.props.currentGoal}
-                        />
+                <div className = "sliders-bar item border-right">
+                    <div className="Row">
+                        <div className="Column current-target">
+                            <div className="current-target-header">Current Target</div>
+                            <br/>
+                            <div className="goals-calories">{this.props.currentGoal.calories}</div>
+                            <div className="calories-label">Calories</div>
+                        </div>
+                        <div className="Column">
+                            <GoalSelector
+                            onGoalChanged={this.props.onGoalChanged}
+                            currentGoal={this.props.currentGoal}
+                            />
+                        </div>
+                    </div>
                 </div>
 
 
-                <div className="meals-amount-bar item border"></div>
-                <div className="run-generation-button item">
-
-                    <div className="round-button"><div className="round-button-circle"><a onClick={this.props.onGoalSubmit} className="round-button">Submit</a></div></div>
+                <div className="meals-amount-bar item border-left">
+                    <div className="Row">
+                        <div className = "meals-amount">
+                            <div className="Row meals-amount-text">
+                                6
+                            </div>
+                            <div className="Row">
+                                <div className="Column meal-part">
+                                    <div className = "meal-selected"></div>
+                                </div>
+                                <div className="Column meal-part">
+                                    <div className = "meal-selected"></div>
+                                </div>
+                                <div className="Column meal-part">
+                                    <div className = "meal-selected"></div>
+                                </div>
+                                <div className="Column meal-part">
+                                    <div className = "meal-selected"></div>
+                                </div>
+                                <div className="Column meal-part">
+                                    <div className = "meal-selected"></div>
+                                </div>
+                                <div className="Column meal-part">
+                                    <div className = "meal-selected"></div>
+                                </div>
+                                <div className="Column meal-part">
+                                    <div className = "meal-empty"></div>
+                                </div>
+                                <div className="Column meal-part">
+                                    <div className = "meal-empty"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="Column">
+                            <div className="run-generation-button ">
+                                <button onClick={this.props.onGoalSubmit} className="run-button">FIND FOOD</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+
             </div>
         );
     }
